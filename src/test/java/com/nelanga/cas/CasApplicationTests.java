cce26fc4597db69babf22286451b0d6c21dd71ef
Nimesh Nelanga <Nimesh.N@CreativeSoftware.com>
2022-03-12 13:39:44 +0530

    Spring boot initialized

---
+++ src/test/java/com/nelanga/cas/CasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelanga.cas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
