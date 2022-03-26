50be226783e9de05d0f8c17ea309c4341216422d
NimeshNelanga-at-988279413178 <Nimesh.N@CreativeSoftware.com>
2022-03-27 01:10:03 +0530

    Fixed issue with MethodArgumentNotValidException handler

--- src/main/java/com/nelanga/cas/exception/CasExceptionHandler.java
+++ src/main/java/com/nelanga/cas/exception/CasExceptionHandler.java
@@ -7,10 +7,13 @@ import com.nelanga.cas.exception.types.CasServiceException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
@@ -24,10 +27,16 @@ public class CasExceptionHandler extends ResponseEntityExceptionHandler {
         return buildResponseEntity(apiError);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<Object> handleValidationIssues(MethodArgumentNotValidException exception) {
+    /*
+     * In conflict with springs ResponseEntityExceptionHandler which already has
+     * @ExceptionHandler(MethodArgumentNotValidException.class)
+     * Therefor need to override default behaviour
+     * */
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception,
+                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
         ApiError apiError = ApiErrorBuilder.build(exception);
-        return buildResponseEntity(apiError);
+        return handleExceptionInternal(exception, apiError, headers, apiError.getResponseStatus(), request);
     }
 
     @ExceptionHandler(Exception.class)
